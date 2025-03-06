7d699f047241c975ad5fda69877d0f9cd4d1acf2
AJM-03 <a.moreland2003@gmail.com>
2025-03-06 13:28:44 +0000

    Scriptable Objects for Traits

--- Assets/Scripts/Shrimp/Gene.cs
+++ Assets/Scripts/Shrimp/Gene.cs
@@ -1,14 +1,14 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public struct Gene
 {
-    public int ID;
-    public string name;
+    public string ID;
     public int dominance;
     public int value;
 }
+
 
 public struct Trait
 {
@@ -21,3 +21,81 @@ public struct Trait
         this.inactiveGene = i;
     }
 }
+
+
+public enum TraitType
+{
+    Colour,
+    Pattern,
+    BodyPart
+}
+
+public enum TraitSet
+{
+    Cherry,
+    Anomalis,
+    Caridid,
+    Nylon
+}
+
+public enum ModifierEffects
+{
+    temperament,
+    geneticSize
+}
+
+[System.Serializable]
+public struct Modifier
+{
+    public ModifierEffects type;
+    [Range(-100, 100)] public float effect;
+}
+
+
+[CreateAssetMenu(fileName = "Trait", menuName = "ScriptableObjects/Trait")]
+public class TraitSO : ScriptableObject
+{
+    public string ID;  // Matches the trait with the saved genes
+    public string traitName;
+
+    [Space(10)]
+    public int minValue;
+    public int maxValue;
+
+    [Space(30)]
+    public List<Modifier> statModifiers;
+
+    [Space(30)]
+    public TraitType type;
+
+    [Header("Colour Trait")]  // Only needs to be filled out if the trait is of type colour
+    public Color color = Color.white;
+
+    [Header("Pattern Trait")]  // Only needs to be filled out if the trait is of type pattern
+    public Sprite texture;
+
+    [Header("Body Part Trait")]  // Only needs to be filled out if the trait is of type body part
+    public GameObject part;
+}
+
+
+/*
+ * List of traits in manager
+ * Create structs in manager for each trait to save values
+ * Function to get the SO using the trait ID
+ * Function to get the manager version of the trait using the trait ID
+ * Modifiers
+ */
+
+
+
+/*
+ * 
+ * IDs
+ * 
+ * Prefix:
+ * C - Colour
+ * P - Pattern
+ * B - Body Part
+ * 
+ */
