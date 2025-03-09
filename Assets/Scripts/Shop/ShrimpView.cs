b774c818cfa26b7af35306b5e39d41f892ac2d1b
Wolf-Sleuth <kesterdersonperson@gmail.com>
2025-03-09 15:31:41 +0000

    Added SOs
    - Added SOs for the traits we have so far
    - Also used the SOs in the ui, so the shrimp view now has more info about the shrimp

--- Assets/Scripts/Shop/ShrimpView.cs
+++ Assets/Scripts/Shop/ShrimpView.cs
@@ -2,6 +2,7 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -14,9 +15,7 @@ public class ShrimpView : ScreenView
     [SerializeField]
     private GameObject tankView;
     [SerializeField]
-    private TextMeshProUGUI age;
-    [SerializeField]
-    private TextMeshProUGUI gender;
+    private TextMeshProUGUI age, gender, body, pattern, legs, tail, tailFan, eyes, head;
     [SerializeField]
     private RenderTexture texture;
     protected Shrimp _shrimp;
@@ -58,6 +57,13 @@ public class ShrimpView : ScreenView
         //title.placeholder.GetComponent<TextMeshProUGUI>().text = _shrimp.stats.name;
         age.text = "Age: " + TimeManager.instance.GetShrimpAge(_shrimp.stats.birthTime).ToString();
         gender.text = "Gender: " + (_shrimp.stats.gender == true ? "M" : "F");
+        pattern.text = "Pattern: " + GeneManager.instance.GetTraitSO(_shrimp.stats.pattern.activeGene.ID).traitName;
+        body.text = "Body: " + GeneManager.instance.GetTraitSO(_shrimp.stats.body.activeGene.ID).set;
+        legs.text = "Legs: " + GeneManager.instance.GetTraitSO(_shrimp.stats.body.activeGene.ID).set;
+        eyes.text = "Eyes: " + GeneManager.instance.GetTraitSO(_shrimp.stats.eyes.activeGene.ID).set;
+        tail.text = "Tail: " + GeneManager.instance.GetTraitSO(_shrimp.stats.tail.activeGene.ID).set;
+        head.text = "Head: " + GeneManager.instance.GetTraitSO(_shrimp.stats.head.activeGene.ID).set;
+        tailFan.text = "Tail Fan: " + GeneManager.instance.GetTraitSO(_shrimp.stats.tailFan.activeGene.ID).set;
         primaryColour.color = GeneManager.instance.GetTraitSO(_shrimp.stats.primaryColour.activeGene.ID).color;
         secondaryColour.color = GeneManager.instance.GetTraitSO(_shrimp.stats.secondaryColour.activeGene.ID).color;
     }
