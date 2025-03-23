b1580067cacd211ce1475f9bfbdb321a61afec2c
Wolf-Sleuth <kesterdersonperson@gmail.com>
2025-03-23 12:33:28 +0000

    Started Main Menu

---
+++ Assets/UIController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+
+    private Button startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        startButton = root.Q<Button>("MainMenuButton");
+        startButton.clicked += StartGame;
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("ShopScene");
+    }
+}
