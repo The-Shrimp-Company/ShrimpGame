c3e0765604fe58d82439d7ab4205d7fb00c31bf6
Wolf-Sleuth <kesterdersonperson@gmail.com>
2025-03-27 17:34:01 +0000

    Formatting fix

--- Assets/Scripts/UI/EmailScreenScripts/ShrimpSelectionBlock.cs
+++ Assets/Scripts/UI/EmailScreenScripts/ShrimpSelectionBlock.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -32,26 +33,32 @@ public class ShrimpSelectionBlock : ContentBlock
         tailFan.text = "Tail Fan: " + GeneManager.instance.GetTraitSO(_shrimp.tailFan.activeGene.ID).set;
         primaryColour.color = GeneManager.instance.GetTraitSO(_shrimp.primaryColour.activeGene.ID).color;
         secondaryColour.color = GeneManager.instance.GetTraitSO(_shrimp.secondaryColour.activeGene.ID).color;
-        price.text = EconomyManager.instance.GetShrimpValue(shrimp).ToSafeString();
+        string thistext = "£" + EconomyManager.instance.GetShrimpValue(shrimp).ToSafeString();
+        string regEx = @"^.*\.\d$";
+        Regex regex = new Regex(regEx);
+        Regex otherregex = new Regex(@"^.*\.\d\d+$");
+        if (thistext.Contains("."))
+        {
+            if (regex.IsMatch(thistext))
+            {
+                price.text = thistext + "0";
+            }
+            else if (otherregex.IsMatch(thistext))
+            {
+                price.text = thistext.Substring(0, thistext.IndexOf(".") + 3);
+            }
+        }
+        else
+        {
+            Debug.Log("HEEEEEEEE");
+            price.text = thistext + ".00";
+        }
     }
 
     public void Populate(ShrimpStats shrimp, ShrimpPurchaseContent par)
     {
         parent = par;
-        _shrimp = shrimp;
-        text.text = _shrimp.name;
-        age.text = "Age: " + (TimeManager.instance.GetShrimpAge(_shrimp.birthTime) < 60 ? "Child" : "Adult");
-        gender.text = "Gender: " + (_shrimp.gender == true ? "M" : "F");
-        pattern.text = "Pattern: " + GeneManager.instance.GetTraitSO(_shrimp.pattern.activeGene.ID).traitName;
-        body.text = "Body: " + GeneManager.instance.GetTraitSO(_shrimp.body.activeGene.ID).set;
-        legs.text = "Legs: " + GeneManager.instance.GetTraitSO(_shrimp.legs.activeGene.ID).set;
-        eyes.text = "Eyes: " + GeneManager.instance.GetTraitSO(_shrimp.eyes.activeGene.ID).set;
-        tail.text = "Tail: " + GeneManager.instance.GetTraitSO(_shrimp.tail.activeGene.ID).set;
-        head.text = "Head: " + GeneManager.instance.GetTraitSO(_shrimp.head.activeGene.ID).set;
-        tailFan.text = "Tail Fan: " + GeneManager.instance.GetTraitSO(_shrimp.tailFan.activeGene.ID).set;
-        primaryColour.color = GeneManager.instance.GetTraitSO(_shrimp.primaryColour.activeGene.ID).color;
-        secondaryColour.color = GeneManager.instance.GetTraitSO(_shrimp.secondaryColour.activeGene.ID).color;
-        price.text = EconomyManager.instance.GetShrimpValue(shrimp).ToSafeString();
+        Populate(shrimp);
     }
 
     public void BuyThis()
