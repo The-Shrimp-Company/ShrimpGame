882095a935246c063e0de505b10f28e9f33d4877
AJM-03 <a.moreland2003@gmail.com>
2025-04-24 13:48:00 +0100

    Clock Display Change

--- Assets/Scripts/UI/TimeUI.cs
+++ Assets/Scripts/UI/TimeUI.cs
@@ -15,6 +15,18 @@ public class TimeUI : MonoBehaviour
 
     void Update()
     {
-        text.text = string.Format("{0:00}:{1:00}", TimeManager.instance.hour, TimeManager.instance.minute);
+        //text.text = string.Format("{0:00}:{1:00}", TimeManager.instance.hour, TimeManager.instance.minute);
+
+
+        int day = TimeManager.instance.day;
+        int hour = Mathf.RoundToInt(TimeManager.instance.hour);
+        string time = "AM";
+        if (hour >= 12) time = "PM";
+        if (hour == 0) hour = 24;
+        if (hour > 12) hour -= 12;
+        if (hour < 10) time = " " + time;
+        if (day == 0) day = day + 1;
+
+        text.text = "Day " + day + "   " + hour + time;
     }
 }
