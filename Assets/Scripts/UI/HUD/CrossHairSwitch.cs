5abbdd97619031f60f546cf83276865c9edb6dfb
AJM-03 <a.moreland2003@gmail.com>
2025-06-03 17:07:07 +0100

    Importing DoTween & Crosshair Changes
    - Imported DoTween
    - Imported Kenney Button Prompt sprites
    - Changed visuals for the crosshair

--- Assets/Scripts/UI/HUD/CrossHairSwitch.cs
+++ Assets/Scripts/UI/HUD/CrossHairSwitch.cs
@@ -3,27 +3,51 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class CrossHairSwitch : MonoBehaviour
 {
     [SerializeField] private Image crosshair;
+    [SerializeField] private TextMeshProUGUI text;
+    private RectTransform crosshairRect;
 
-    private TextMeshProUGUI text;
+    [SerializeField] private Sprite crosshairSprite;
+    [SerializeField] private float crosshairSize = 1f;
+
+    [SerializeField] private Sprite interactSprite;
+    [SerializeField] private float interactSize = 1f;
+
+    [SerializeField] private Ease crosshairEase;
 
     private void Start()
     {
-        text = GetComponent<TextMeshProUGUI>();
+        crosshairRect = crosshair.GetComponent<RectTransform>();
     }
 
     private void Update()
     {
         if(text.text == "" && text.enabled)
         {
-            crosshair.enabled = true;
+            if (crosshair.sprite != crosshairSprite)
+            {
+                ChangeSprite(crosshairSprite, crosshairSize);
+                text.DOFade(0, 0.1f);
+            }
         }
         else
         {
-            crosshair.enabled = false;
+            if (crosshair.sprite != interactSprite)
+            {
+                ChangeSprite(interactSprite, interactSize);
+                text.DOFade(1, 0.1f);
+            }
         }
+    }
+
+    private void ChangeSprite(Sprite sprite, float size)
+    {
+        crosshair.sprite = sprite;
+        DOTween.Kill(crosshairRect);
+        crosshairRect.DOScale(new Vector2(size, size), 0.2f).SetEase(crosshairEase);
     }
 }
